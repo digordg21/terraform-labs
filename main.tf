c5838ec743f3454989452cea67b7a1688d7d44cc
Abed De Andrade <rabed@indracompany.com>
2022-12-28 19:16:41 -0300

    criando repo terraform

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "abed_IaC_server" {
+  ami           = "ami-0530ca8899fac469f"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "primeira instancia"
+  }
+}
