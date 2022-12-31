551260ff4c7b3dc314b5e239480b001661440507
Abed De Andrade <rabed@indracompany.com>
2022-12-31 15:39:49 -0300

    teste 2

--- main.tf
+++ main.tf
@@ -17,12 +17,6 @@ resource "aws_instance" "abed_IaC_server" {
   ami           = "ami-0530ca8899fac469f"
   instance_type = "t2.micro"
   key_name = "iac-abed"
-  user_data = <<-EOF
-                #!/bin/bash
-                cd /home/ubuntu
-                echo "<h1>Terraform OLA MUNDO</h1>" > index.html
-                nohup busybox -httpd -f -p 8080 &
-                EOF
   tags = {
     Name = "teste com index.html"
   }
